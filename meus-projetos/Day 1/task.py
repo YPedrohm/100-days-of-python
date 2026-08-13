186d05e3ac0967dd579398622fffbe8e9b9af168
Pedro <pedrohhmmoliveira@gmail.com>
2026-08-12 21:43:13 -0300

    Dia 1 - Python

---
+++ meus-projetos/Day 1/task.py
@@ -0,0 +1,4 @@
+print("Olá! bem vindo ao gerador de nome de bandas")
+cidade = input("Qual é o nome da cidade em que você cresceu?\n")
+nomepet = input("Qual é o nome do seu animal de estimação?\n")
+print("o nome da sua banda pode ser ", cidade + " " + nomepet)
